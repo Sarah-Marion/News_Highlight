51423a5f1b7dd376f97f907c9c8c18bd92dd2920
Sarah-Marion <devsarahmarion@gmail.com>
2018-05-12 14:11:57 +0300

    created a function that processes the result list and transforms them into a list of objects

--- app/requests.py
+++ app/requests.py
@@ -24,5 +24,37 @@ def get_sources(source):
         get_sources_data = url.read()
         get_sources_response = json.loads(get_sources_data)
 
-        Sources_results = None
-        
+        sources_results = None
+
+        if get_sources_response["sources"]:
+            sources_results_list = get_sources_response["sources"]
+            sources_results = process_results(sources_results_list)
+
+    return sources_results
+
+
+def process_results(source_list):
+    """
+    Function that processes the results list and transforms them into a list of objects
+
+    Args:
+        sources_list: A list of dictionaries that contains news source details
+    Returns:
+        source_results: A list of news souce objects
+    """
+    sources_results = []
+
+    for source_item in sources_list:
+        id = source_item.get('id')
+        name = source_item.get('name')
+        url = source_item.get('url')
+        description = source_item.get('description')
+        cateory = source_item.get('category')
+
+
+        source_object = Sources(id, name, url, description, category)
+        sources_results.append(source_object)
+
+    return sources_results
+
+
