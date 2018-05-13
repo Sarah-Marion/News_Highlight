51bd930aed26a8166d487be2229bc21d459ff1c0
Sarah-Marion <devsarahmarion@gmail.com>
2018-05-13 21:06:10 +0300

    fixed indentation error

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ class Config:
     """
     General configuration parent class
     """
-
+    # pass
     NEWS_API_BASE_URL = 'https://newsapi.org/v2/sources?language=en&category={}&apiKey={}'
     NEWS_API_KEY = os.environ.get("NEWS_API_KEY")
     SECRET_KEY =  os.environ.get("SECRET_KEY")
